72014501457e90761062b55fd68e18431ff62f4f
DEEPAK <deepakramesh1583@gmail.com>
2025-04-06 11:04:58 +0530

    today updated

---
+++ backend/src/controlers/auth.controller.js
@@ -0,0 +1,41 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+export const signup = async (req, res)=>{
+   const {fullName,email,password} = req.body;
+   //hashing the password
+   try {
+   
+    if(password.length<6)
+    {
+        return res.status(400).json({message:"password must be at least 6 characters"});
+    }
+
+        const user = await User.findOne({email})
+        if(user) return res.status(400).json({message: "email already exits"}); 
+
+        const salt = await bcrypt.genSalt(10)
+        const hashPassword = await bcrypt.hash(password,salt)
+
+        const newUser = new User({
+            fullName,
+            email,
+            password:hashedPassword,
+        })
+
+        if(newUser){
+            //generate jwt token 
+
+        }
+        else{
+            res.status(400).json({message: "invalid user data"});
+        }
+   } catch (error) 
+   {
+   }
+};
+export const login = (req, res)=>{
+    res.send("login route");
+};
+export const logout = (req, res)=>{
+    res.send("logout route");
+};
